c27bf16092a5e04df6a7a9fb09c39bcff4ae9e27
allekevi <36173629+allekevi@users.noreply.github.com>
2019-01-23 13:40:13 -0800

    First commit

---
+++ projectTest.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+//var mysql = require('./dbcon.js');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/public', express.static('public'));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 1000);
+//app.set('mysql', mysql);
+
+app.get('/', function(req, res){
+  res.render('genHome');
+})
+
+app.use('/userHome', require('./userHome.js'));
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+  });
+  
+  app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+  });
+
+  app.listen(app.get('port'), function(){
+    console.log('Express started on localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  });
