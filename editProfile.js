b1d14012461baff5b584a017fe48b065b25b2425
allekevi <36173629+allekevi@users.noreply.github.com>
2019-01-30 13:50:44 -0800

    Edit profile added/alt layout

---
+++ editProfile.js
@@ -0,0 +1,6 @@
+function editProfile(){
+    $.ajax({
+        url: '/editProfile',
+        
+    })
+} 
--- userHome.js
+++ userHome.js
@@ -1,9 +1,13 @@
 module.exports = function(){
     var express = require('express');
     var router = express.Router();
-
+    
     router.get('/', function(req, res){
-        res.render('userHome');
+        res.render('userHome', {layout : 'user'});
+    });
+
+    router.get('/editProfile', function(req, res){
+        res.render('editProfile', {layout : 'user'});
     });
     return router;
 
