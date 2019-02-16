9220e0df83202eda341cb01c48f07875d52c9198
curranc86 <curranc86@gmail.com>
2019-02-15 20:04:34 -0600

    Add files via upload

---
+++ adduseraward1.sql
@@ -0,0 +1,32 @@
+INSERT into user_awards (user_id, award_id, award_date, award_comments, created_by, created_date, modified_by,
+						 last_modified, active_flag) values
+						("4", "2", "2019-02-10", "Perfect job!", "1", "2019-02-10", "1", "2019-02-10", "1"),
+                        ("47", "5", "2019-02-11", null, "1", "2019-02-11", "1", "2019-02-11", "1"),
+                        ("10", "3", "2019-02-10", "Great coordination with the team", "1", "2019-02-10",
+                        "1", "2019-02-10", "0"),
+                        ("5", "1", "2019-02-10", "You helped me a lot when I didn't have the time.  Thanks!",
+                        "4", "2019-02-10", "4", "2019-02-10", "1"),
+                        ("5", "6", "2019-02-10", null, "2", "2019-02-10", "2", "2019-02-10", "0"),
+                        ("5", "2", "2019-02-10", "Perfect job!", "1", "2019-02-10", "1", "2019-02-10", "1"),
+                        ("23", "7", "2019-02-10", "Your coworkers always have great comments about your work 
+                        and teamwork.  Keep it up!", "1", "2019-02-10", "1", "2019-02-10", "0"),
+                        ("4", "2", "2019-02-10", "Perfect job again!", "1", "2019-02-10", "12", "2019-02-10", "12"),
+                        ("47", "7", "2019-02-11", "Your coworkers always have great comments about your work 
+                        and teamwork.  Keep it up!", "1", "2019-02-11", "1", "2019-02-10", "1"),
+                        ("18", "4", "2019-02-10", "You breadth of skills makes you an asset to our team!", "1",
+                        "2019-02-10", "1", "2019-02-10", "1"),
+                        ("15", "5", "2019-02-09", "If you were an faster, you'd be a race car!", "1", "2019-02-09",
+                        "1", "2019-02-09", "0"),
+                        ("12", "10", "2019-02-09", null, "5", "2019-02-09", "5", "2019-02-09", "1"),
+                        ("1", "9", "2019-02-10", "Thanks for the help whenever it is needed.  You are always
+                        willing to lend a helping hand to the team", "2", "2019-02-10", "2", "2019-02-10", "0"),
+                        ("1", "8", "2019-02-10", "Thanks for the help whenever it is needed.  You are always
+                        willing to lend a helping hand to the team", "2", "2019-02-10", "2", "2019-02-10", "1"),
+                        ("4", "9", "2019-02-09", null, "1", "2019-02-09", "1", "2019-02-10", "0"),
+                        ("14", "2", "2019-02-10", "Perfect job!", "10", "2019-02-10", "10", "2019-02-10", "1"),
+                        ("8", "1", "2019-02-09", "Thanks!", "15", "2019-02-09", "15", "2019-02-09", "1"),
+                        ("19", "2", "2019-02-11", "Perfect job!", "1", "2019-02-11", "1", "2019-02-11", "1"),
+                        ("4", "1", "2019-02-10", "Gracias!", "18", "2019-02-10", "18", "2019-02-10", "1"),
+                        ("4", "2", "2019-02-10", "Perfect job!", "2", "2019-02-10", "2", "2019-02-10", "1"),
+                        ("5", "1", "2019-02-09", "Thank you!", "3", "2019-02-09", "3", "2019-02-09", "1");
+                        
